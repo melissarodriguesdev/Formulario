b38610393b384ecd25cf834c6faadc3b98ec4553
melissarodriguesdev <melissabarbosa3008@gmail.com>
2026-03-23 20:56:59 -0300

    Create teste.php with a form for user input

---
+++ teste.php
@@ -0,0 +1,44 @@
+<?php
+$name = "";
+$email = "";
+$website = "";
+$comment = "";
+$gender = "";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>teste</title>
+</head>
+<body>
+
+<form method="post" action="">
+
+Name: <input type="text" name="name" value="<?php echo $name;?>">
+
+E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+
+Website: <input type="text" name="website" value="<?php echo $website;?>">
+
+Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+
+Gender:
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="female") echo "checked";?>
+value="female">Female
+
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="male") echo "checked";?>
+value="male">Male
+
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="other") echo "checked";?>
+value="other">Other
+
+</form>
+
+</body>
+</html>
